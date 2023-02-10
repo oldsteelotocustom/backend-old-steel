fbbfed24406b02ea6415d45d9685be482b8853fb
oldsteelotocustom <oldsteelotocustom@gmail.com>
2023-02-10 11:43:04 +0700

    Add Dependency Injection to its repository

--- src/main/java/com/oldsteel/service/CategoryService.java
+++ src/main/java/com/oldsteel/service/CategoryService.java
@@ -1,7 +1,12 @@
 package com.oldsteel.service;
 
+import com.oldsteel.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CategoryService {
+
+    private final CategoryRepository categoryRepo;
 }
