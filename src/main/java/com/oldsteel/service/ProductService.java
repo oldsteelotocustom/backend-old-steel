7e5a9000e937b76f5cc87653f8915a5b5f033e9f
oldsteelotocustom <oldsteelotocustom@gmail.com>
2023-02-10 15:39:29 +0700

    Create new method

--- src/main/java/com/oldsteel/service/ProductService.java
+++ src/main/java/com/oldsteel/service/ProductService.java
@@ -1,7 +1,18 @@
 package com.oldsteel.service;
 
+import com.oldsteel.entity.Product;
+import com.oldsteel.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ProductService {
+
+    private final ProductRepository productRepo;
+
+    public Product saveProduct(Product product){
+        product.setAvailability(true);
+        return productRepo.save(product);
+    }
 }
