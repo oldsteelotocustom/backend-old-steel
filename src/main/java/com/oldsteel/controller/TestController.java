92711dfd71d68e939c391b232d4ffdd48f8bfa71
oldsteelotocustom <oldsteelotocustom@gmail.com>
2023-02-10 11:10:53 +0700

    Make test controller

--- src/main/java/com/oldsteel/controller/TestController.java
+++ src/main/java/com/oldsteel/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.oldsteel.oto.controller;
+package com.oldsteel.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
