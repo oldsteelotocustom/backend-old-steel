5b820dac966aa2227e60b7721de10c34b79b014a
Rainhardvidiansyah <rainhardvidians@gmail.com>
2023-02-10 21:45:52 +0700

    Create method to save Category

--- src/main/java/com/oldsteel/service/CategoryService.java
+++ src/main/java/com/oldsteel/service/CategoryService.java
@@ -1,5 +1,6 @@
 package com.oldsteel.service;
 
+import com.oldsteel.entity.Category;
 import com.oldsteel.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,8 @@ import org.springframework.stereotype.Service;
 public class CategoryService {
 
     private final CategoryRepository categoryRepo;
+
+    public Category createCategory(Category category){
+        return categoryRepo.save(category);
+    }
 }
