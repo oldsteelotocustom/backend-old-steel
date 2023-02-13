b2199776b75f9b38203bc46c977c46c2f46db64e
Rainhardvidiansyah <rainhardvidians@gmail.com>
2023-02-13 15:35:33 +0700

    Create new method insert product to category

--- src/main/java/com/oldsteel/service/CategoryService.java
+++ src/main/java/com/oldsteel/service/CategoryService.java
@@ -1,17 +1,34 @@
 package com.oldsteel.service;
 
 import com.oldsteel.entity.Category;
+import com.oldsteel.entity.Product;
 import com.oldsteel.repository.CategoryRepository;
+import com.oldsteel.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CategoryService {
 
     private final CategoryRepository categoryRepo;
+    private final ProductRepository productRepo;
 
     public Category createCategory(Category category){
         return categoryRepo.save(category);
+    }
+
+    public void insertBookToCategory(Long categoryId, Long productId){
+        var product = productRepo.findById(productId)
+                .orElseThrow(() -> new RuntimeException("Product not found..."));
+        var category = categoryRepo.findById(categoryId)
+                .orElseThrow(() -> new RuntimeException("Category not found... Please create one"));
+        List<Product> productList = new ArrayList<>();
+        productList.add(product);
+        category.setProducts(productList);
+        categoryRepo.save(category);
     }
 }
