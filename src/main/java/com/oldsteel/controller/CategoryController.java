cdc6f74c8928978df3708d9d5b8a523009081bfb
Rainhardvidiansyah <rainhardvidians@gmail.com>
2023-02-13 15:36:07 +0700

    Create new method to insert product to category in CategoryController

--- src/main/java/com/oldsteel/controller/CategoryController.java
+++ src/main/java/com/oldsteel/controller/CategoryController.java
@@ -1,16 +1,14 @@
 package com.oldsteel.controller;
 
 import com.oldsteel.dto.request.CategoryRequestDto;
+import com.oldsteel.dto.request.ProductToCategoryRequestDto;
 import com.oldsteel.entity.Category;
 import com.oldsteel.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/category")
@@ -22,7 +20,19 @@ public class CategoryController {
 
     @PostMapping("/save")
     public ResponseEntity<?> saveCategory(@RequestBody CategoryRequestDto categoryDto){
-        categoryService.createCategory(Category.saveFrom(categoryDto));
+        var category = categoryService.createCategory(Category.saveFrom(categoryDto));
         return new ResponseEntity<>("Category saved successfully", HttpStatus.CREATED);
+    }
+
+    @PutMapping("/add-product")
+    public ResponseEntity<?> insertProductToCategory(@RequestBody ProductToCategoryRequestDto toCategoryRequestDto){
+        if(toCategoryRequestDto.getProductId().equals(0L)){
+            return new ResponseEntity<>("", HttpStatus.BAD_REQUEST);
+        } else if (toCategoryRequestDto.getCategoryId().equals(0L)) {
+            return new ResponseEntity<>("", HttpStatus.BAD_REQUEST);
+        }
+        categoryService.insertBookToCategory(toCategoryRequestDto.getCategoryId(),
+                toCategoryRequestDto.getProductId());
+        return new ResponseEntity<>("Category has been added to product...", HttpStatus.OK);
     }
 }
