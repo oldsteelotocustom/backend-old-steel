e15c31169180f19614a742d14b023c40c3d28c9f
Rainhardvidiansyah <rainhardvidians@gmail.com>
2023-02-13 12:57:31 +0700

    Add JsonProperty

--- src/main/java/com/oldsteel/dto/request/ProductToCategoryRequestDto.java
+++ src/main/java/com/oldsteel/dto/request/ProductToCategoryRequestDto.java
@@ -1,11 +1,15 @@
 package com.oldsteel.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 @Getter @Setter
 @AllArgsConstructor @NoArgsConstructor @ToString
 public class ProductToCategoryRequestDto {
 
+    @JsonProperty("category_id")
     private Long categoryId;
+
+    @JsonProperty("product_id")
     private Long productId;
 }
