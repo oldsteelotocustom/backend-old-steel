4d5c187528136a96f195af638162ed0cc0176b9f
Rainhardvidiansyah <rainhardvidians@gmail.com>
2023-02-14 14:28:29 +0700

    Add method add product to category

--- src/main/java/com/oldsteel/service/ProductService.java
+++ src/main/java/com/oldsteel/service/ProductService.java
@@ -1,17 +1,22 @@
 package com.oldsteel.service;
 
 import com.oldsteel.entity.Product;
+import com.oldsteel.repository.CategoryRepository;
 import com.oldsteel.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ProductService {
 
     private final ProductRepository productRepo;
+    private final CategoryRepository categoryRepo;
 
     public Product saveProduct(Product product){
         product.setAvailability(true);
@@ -20,5 +25,14 @@ public class ProductService {
 
     public List<Product> getAllProducts(){
         return productRepo.findAll();
+    }
+
+    @Transactional
+    public void insertBookToCategory(Long productId, Long categoryId){
+        var product = productRepo.findById(productId);
+        var category = categoryRepo.findById(categoryId);
+        product.get().getCategories().add(category.get());
+        productRepo.save(product.get());
+        log.info("Updated in categories of product!");
     }
 }
