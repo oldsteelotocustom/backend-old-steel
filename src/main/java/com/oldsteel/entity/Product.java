843a2648b1506d360df7f4a7ed15c6243914d66f
Rainhardvidiansyah <rainhardvidians@gmail.com>
2023-02-14 14:30:32 +0700

    Add relationship to category

--- src/main/java/com/oldsteel/entity/Product.java
+++ src/main/java/com/oldsteel/entity/Product.java
@@ -1,5 +1,6 @@
 package com.oldsteel.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.oldsteel.dto.request.ProductRequestDto;
 import com.oldsteel.helper.BigDecimalConverter;
 import lombok.AllArgsConstructor;
@@ -9,6 +10,8 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Getter @Setter
@@ -25,10 +28,19 @@ public class Product {
     private boolean availability;
     private int stocks;
 
+    @JsonBackReference
+    @ManyToMany (fetch = FetchType.LAZY,
+            cascade = {CascadeType.PERSIST})
+    @JoinTable(name = "categories_product_tbl",
+            joinColumns = @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories = new HashSet<>();
+
     public static Product saveFrom(ProductRequestDto productDto){
         var product = new Product();
         product.setProductName(productDto.getProductName());
         product.setProductPrice(productDto.getProductPrice());
+        product.setStocks(productDto.getStocks());
         return product;
     }
 }
