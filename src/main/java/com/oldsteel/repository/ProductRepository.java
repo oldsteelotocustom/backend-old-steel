301f3149067e66be43beab7ac2705aa6cdb59fa5
Rainhardvidiansyah <rainhardvidians@gmail.com>
2023-02-15 12:36:23 +0700

    Create query to find product code

--- src/main/java/com/oldsteel/repository/ProductRepository.java
+++ src/main/java/com/oldsteel/repository/ProductRepository.java
@@ -2,8 +2,17 @@ package com.oldsteel.repository;
 
 import com.oldsteel.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Optional<Product> findProductByProductCode(String productCode);
+
+    @Query("SELECT p FROM Product p WHERE p.productCode = :productCode")
+    Product findCode(@Param("productCode") String code);
 }
