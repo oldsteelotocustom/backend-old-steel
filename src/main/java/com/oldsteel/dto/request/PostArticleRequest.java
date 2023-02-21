704bd823ee8ad1978f85fd87423dfd86642ace9c
Rainhardvidiansyah <rainhardvidians@gmail.com>
2023-02-21 12:47:55 +0700

    delete Author property

--- src/main/java/com/oldsteel/dto/request/PostArticleRequest.java
+++ src/main/java/com/oldsteel/dto/request/PostArticleRequest.java
@@ -2,12 +2,10 @@ package com.oldsteel.dto.request;
 
 import lombok.*;
 
-
 @AllArgsConstructor @NoArgsConstructor
 @Getter @Setter @ToString
 public class PostArticleRequest {
 
     private String title;
     private String body;
-    private String author;
 }
