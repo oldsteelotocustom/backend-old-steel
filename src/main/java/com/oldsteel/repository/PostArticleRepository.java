6ab7da04e11dc3f880515a791e2a4a80ed303339
Rainhardvidiansyah <rainhardvidians@gmail.com>
2023-02-23 11:48:04 +0700

    Edit file Post repository

--- src/main/java/com/oldsteel/repository/PostArticleRepository.java
+++ src/main/java/com/oldsteel/repository/PostArticleRepository.java
@@ -4,7 +4,6 @@ import com.oldsteel.entity.PostArticle;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.awt.print.Pageable;
 import java.util.List;
 
 @Repository
