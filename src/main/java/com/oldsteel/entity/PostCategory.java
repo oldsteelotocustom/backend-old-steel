f876da7746336987b3b158f5894490a5055cb430
Rainhardvidiansyah <rainhardvidians@gmail.com>
2023-02-25 10:47:49 +0700

    create post category class

---
+++ src/main/java/com/oldsteel/entity/PostCategory.java
@@ -0,0 +1,28 @@
+package com.oldsteel.entity;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "post_category")
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
+public class PostCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String categoryName;
+
+    @JsonManagedReference
+    @ManyToMany(mappedBy = "postCategories",
+            cascade = {CascadeType.ALL}, fetch = FetchType.EAGER)
+    private Set<PostArticle> postArticles = new HashSet<>();
+}
