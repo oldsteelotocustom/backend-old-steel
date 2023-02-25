b7ae631f822a043ecf64452a2ce5b821c9664efb
Rainhardvidiansyah <rainhardvidians@gmail.com>
2023-02-25 11:01:48 +0700

    Create Post category repository

---
+++ src/main/java/com/oldsteel/repository/PostCategoryRepository.java
@@ -0,0 +1,9 @@
+package com.oldsteel.repository;
+
+import com.oldsteel.entity.PostCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostCategoryRepository extends JpaRepository<PostCategory, Long> {
+}
