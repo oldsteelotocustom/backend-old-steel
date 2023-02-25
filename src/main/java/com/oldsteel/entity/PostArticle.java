e6f6f0c32bc6d0be3fa58290ee347a3644f10d31
Rainhardvidiansyah <rainhardvidians@gmail.com>
2023-02-25 10:48:14 +0700

    Add relationship to post category class

--- src/main/java/com/oldsteel/entity/PostArticle.java
+++ src/main/java/com/oldsteel/entity/PostArticle.java
@@ -1,9 +1,13 @@
 package com.oldsteel.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.oldsteel.dto.request.PostArticleRequest;
 import lombok.*;
 
 import javax.persistence.*;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @AllArgsConstructor @NoArgsConstructor
@@ -23,6 +27,21 @@ public class PostArticle {
 
     @Temporal(TemporalType.TIMESTAMP)
     private Date creationDate;
+
+    public static PostArticle saveFrom(PostArticleRequest postRequest){
+        var post = new PostArticle();
+        post.setTitle(postRequest.getTitle());
+        post.setBody(postRequest.getBody());
+        return post;
+    }
+
+    @JsonBackReference
+    @ManyToMany (fetch = FetchType.LAZY,
+            cascade = {CascadeType.PERSIST})
+    @JoinTable(name = "post_categories_tbl",
+            joinColumns = @JoinColumn(name = "post_id"),
+            inverseJoinColumns = @JoinColumn(name = "post_category_id"))
+    private Set<PostCategory> postCategories = new HashSet<>();
 
 
 
