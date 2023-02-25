a1a7b3099a5ab862f6dcb54e866e730615e51521
Rainhardvidiansyah <rainhardvidians@gmail.com>
2023-02-25 13:14:29 +0700

    Edit file article response

--- src/main/java/com/oldsteel/dto/response/PostArticleResponseDto.java
+++ src/main/java/com/oldsteel/dto/response/PostArticleResponseDto.java
@@ -1,18 +1,19 @@
 package com.oldsteel.dto.response;
 
 import com.oldsteel.entity.PostArticle;
+import com.oldsteel.entity.PostCategory;
 import lombok.*;
 
-import java.util.Date;
+import java.util.*;
+import java.util.stream.Collectors;
 
-@AllArgsConstructor
-@NoArgsConstructor
 @Getter @Setter @ToString
 public class PostArticleResponseDto {
     private String title;
     private String body;
     private String author;
     private Date creationDate;
+    private List<String> categories = new ArrayList<>();
 
     public static PostArticleResponseDto dataFrom(PostArticle postArticle){
         var postResponse = new PostArticleResponseDto();
@@ -20,6 +21,9 @@ public class PostArticleResponseDto {
         postResponse.setBody(postArticle.getBody());
         postResponse.setAuthor(postArticle.getAuthor());
         postResponse.setCreationDate(postArticle.getCreationDate());
+        postResponse.setCategories(postArticle.getPostCategories()
+                        .stream().map(PostCategory::getCategoryName)
+                        .collect(Collectors.toList()));
         return postResponse;
     }
 
