2f9e97469d426e52df93e324e3b38f36a9e409e1
Rainhardvidiansyah <rainhardvidians@gmail.com>
2023-03-07 11:37:09 +0700

    Validate properties in Post article dto

--- src/main/java/com/oldsteel/dto/request/PostArticleRequest.java
+++ src/main/java/com/oldsteel/dto/request/PostArticleRequest.java
@@ -2,12 +2,18 @@ package com.oldsteel.dto.request;
 
 import lombok.*;
 
-import javax.persistence.Lob;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @AllArgsConstructor @NoArgsConstructor
 @Getter @Setter @ToString
 public class PostArticleRequest {
 
+    @NotBlank(message = "Title name must be written")
+    @Size(min = 20, max = 100, message = "Min title must be 20 characters")
     private String title;
+
+    @NotBlank(message = "Body of article or post cannot be blank")
+    @Size(min = 500, max = 2000, message = "Min characters for body of post must be 500")
     private String body;
 }
