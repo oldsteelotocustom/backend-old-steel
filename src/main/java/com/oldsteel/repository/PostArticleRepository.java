551c81cd4719be6fd2cf9c294c60382c751bb3e6
Rainhardvidiansyah <rainhardvidians@gmail.com>
2023-03-07 14:20:18 +0700

    Add method find by title

--- src/main/java/com/oldsteel/repository/PostArticleRepository.java
+++ src/main/java/com/oldsteel/repository/PostArticleRepository.java
@@ -5,9 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface PostArticleRepository extends JpaRepository<PostArticle, Long> {
 
     List<PostArticle> findPostArticleOrderByCreationDate(String title);
+
+    Optional<PostArticle> findPostArticleByTitle(String title);
 }
