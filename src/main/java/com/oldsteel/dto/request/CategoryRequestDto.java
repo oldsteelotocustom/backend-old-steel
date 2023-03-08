45e8dd1aa53bbd505d83c77c51a9ae9d7bb2aaa7
Rainhardvidiansyah <rainhardvidians@gmail.com>
2023-03-08 11:01:29 +0700

    add validator in category request dto

--- src/main/java/com/oldsteel/dto/request/CategoryRequestDto.java
+++ src/main/java/com/oldsteel/dto/request/CategoryRequestDto.java
@@ -5,9 +5,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Getter @Setter
 @AllArgsConstructor @NoArgsConstructor
 public class CategoryRequestDto {
 
+    @NotBlank(message = "Product category name cannot be blank")
+    @Size(min = 20, message = "The length must be at least 20 characters")
     private String categoryName;
 }
