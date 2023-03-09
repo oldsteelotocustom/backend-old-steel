4888382e0df2605e7205ea14439134ccc10abcb9
Rainhardvidiansyah <rainhardvidians@gmail.com>
2023-03-09 12:53:53 +0700

    edit file our client response dto

--- src/main/java/com/oldsteel/dto/response/OurClientNameResponseDto.java
+++ src/main/java/com/oldsteel/dto/response/OurClientNameResponseDto.java
@@ -2,13 +2,10 @@ package com.oldsteel.dto.response;
 
 
 import com.oldsteel.entity.OurClient;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter @Setter
-@AllArgsConstructor @NoArgsConstructor
+@AllArgsConstructor @NoArgsConstructor @ToString
 public class OurClientNameResponseDto {
 
     private String ourClientName;
